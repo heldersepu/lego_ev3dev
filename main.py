f36cc1135b3e8dd544309a58ac8224b9a25ba763
Helder Sepulveda <heldersepu@gmail.com>
2019-11-24 11:03:43 -0500

    class Motors

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ from time import sleep
 from ev3dev2.sound import Sound
 from ev3dev2.motor import LargeMotor
 
+
+class Motors:
+    def __init__(self, ports):
+        self.motors = [LargeMotor(address) for address in ports]
+
+    def move(self, s1, s2, t):
+        self.motors[0].run_timed(speed_sp=s1, time_sp=t)
+        self.motors[1].run_timed(speed_sp=s2, time_sp=t)
+        sleep((t-10)/1000)
+
+
 sound = Sound()
 sound.speak('go!')
 
-motors = [LargeMotor(address) for address in ('outB', 'outC')]
+motors = Motors(('outB', 'outC'))
 
-motors[0].run_timed(speed_sp=600, time_sp=5000)
-motors[1].run_timed(speed_sp=800, time_sp=5000)
-
-while any(m.state for m in motors):
-    sleep(0.1)
+for i in range(2):
+    for i in range(2):
+        motors.move(600, 800, 3000)
+        motors.move(800, 600, 3000)
+    motors.move(100, 900, 2400)
 
 sound.beep()
